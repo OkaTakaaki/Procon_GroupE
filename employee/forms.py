66daafe638237541b049e8ea90f05cbf6d30274e
hikaruaraki <fko2347052@stu.o-hara.ac.jp>
2024-11-19 14:35:30 +0900

    new_seat あとcss

--- employee/forms.py
+++ employee/forms.py
@@ -1,6 +1,37 @@
-# from django import forms
-# from reception_system.models import Seat
 
-# class SeatForm(forms.ModelForm):
-#     class Meta:
-#         model = Seat
+from django import forms
+
+class SeatForm(forms.Form):
+    TABLE_TYPE_CHOICES = [
+        (1, 'テーブル席'),
+        (2, 'カウンター席')
+        
+    ]
+    table_type = forms.ChoiceField(
+        choices=TABLE_TYPE_CHOICES, 
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label = '座席種類'
+    )
+    
+    table_number = forms.CharField(
+        max_length=100, 
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'テーブル番号を入力'}),
+        label = 'テーブル番号'
+    )
+
+    SEATS_CHOICES = [
+        (1, '1人席'),
+        (2, '2人席'),
+        (4, '4人席')
+    ]
+    seats = forms.ChoiceField(
+        choices=SEATS_CHOICES, 
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label = '座席人数'
+    )
+
+    connection = forms.ChoiceField(
+        choices=[(True, '座席連結する'), (False, '座席連結しない')], 
+        widget=forms.RadioSelect(attrs={'class': 'form-control'}),
+        label = '連結する'
+    )
--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from reception_system.models import Seat
+from .forms import SeatForm
 
 # 従業員用ページ
 def employee(request):
@@ -14,25 +15,40 @@ def confirm(request):
     tables = Seat.objects.all()
     return render(request, 'employee/confirm.html', {'tables': tables})
 
-# 新しい座席の登録ページ
+# # 新しい座席の登録ページ
+# def new_seat(request):
+#     if request.method == 'POST':
+#         # フォームから送信されたデータを取得
+#         table_type = request.POST.get('table_type')
+#         table_number = request.POST.get('table_number')
+#         recommended_capacity = request.POST.get('recommended_capacity')
+#         table_connect = request.POST.get('connection')
+
+#         # 入力されたデータを保存
+#         new_seat = Seat.objects.create(
+#             table_type=table_type,
+#             table_number=table_number,
+#             recommended_capacity=recommended_capacity,
+#             table_connect=(table_connect == 'True'),  # 'True' の場合は連結する
+#             table_resevation=False,  # 最初は使用されていないとしてデフォルトでFalse
+#             electrical_outlet=False,  # コンセント有無はデフォルトでFalse
+#             clean=False,  # 清掃済みはデフォルトでFalse
+#         )
+#         new_seat.save()
+
+#         # データ保存後に確認ページへリダイレクト
+#         return redirect('confirm')
+
+#     return render(request, 'employee/new_seat.html')
+
 def new_seat(request):
-    if request.method == 'POST':
-        # フォームから送信されたデータを取得
-        status = request.POST.get('status')
-        table_number = request.POST.get('table_number')
-        recommended_capacity = request.POST.get('seats')
-        connection = request.POST.get('connection')
+    if request.method =='POST':
+        form = SeatForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('employee_new_seat')
+    
+    else:
+        form = SeatForm()
 
-        # 新しいテーブル情報を保存
-        Seat.objects.create(
-            table_type=status,
-            table_number=table_number,
-            recommended_capacity=recommended_capacity,
-            table_connect=connection,
-            table_resevation=False
-        )
-
-        # データ保存後に確認ページへリダイレクト
-        return redirect('confirm')
-
-    return render(request, 'employee/new_seat.html')
+    return render(request, 'employee/new_seat.html',{'form': form})
