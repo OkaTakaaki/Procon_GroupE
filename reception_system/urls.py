71e1e5fe8e65efc4d8077be5b95ee108e9b671a4
Takaaki Oka <fko2347014@stu.o-hara.ac.jp>
2024-11-19 10:16:28 +0900

    受付番号作成

--- reception_system/urls.py
+++ reception_system/urls.py
@@ -5,7 +5,7 @@ app_name = 'reception_system'
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('reception/', views.reception, name='reception'),
+    path('select_num_people/', views.select_num_people, name='select_num_people'),
     path('language/', views.language, name='language'),
     path('complate/', views.complate, name='reception_complate'),
 ]
--- reception_system/views.py
+++ reception_system/views.py
@@ -1,35 +1,90 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Reception
-from .forms import ReceptionCountForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import NumPeopleForm, SeatingTypeForm, SeatSpecificationForm
+import re
 
 def index(request):
     return render(request,'reception_system/index.html')
 
-def reception(request):
-    if request.method == 'POST':
-        # フォームのインスタンスを作成し、POSTデータを渡す
-        receptionform = ReceptionCountForm(request.POST)
-        if receptionform.is_valid():
-            print("aaaaaa")
-            # データベースには保存せずに、インスタンスのみを生成
-            reception = receptionform.save(commit=False)
-            print(reception)
-            # 最終的にデータベースに保存
-            reception.save()
-            print("データが保存されました")
-            return render(request, 'reception_system/reception.html', {'message': 'データが正常に保存されました'})
-        else:
-            # バリデーションエラーが発生した場合はエラーメッセージとともにフォームを再表示
-            return render(request, 'reception_system/reception.html', {'form': receptionform})
+def select_reception_number():
+    reception = Reception.objects.all()
+    last = reception.last()
+    if last == None:
+        reception_number = "M-1"
     else:
-        # GETリクエストの場合は空のフォームを渡す
-        receptionform = ReceptionCountForm()
-        return render(request, 'reception_system/reception.html', {'form': receptionform})
+        last_reception_number = last.reception_number
+        result = re.sub(r'[^0-9]', '', last_reception_number)
+        print("--------{}-------".format(result))
+        if len(result) == 1:
+            print("A")
+            reception_number = "M-" + str(int(result) + 1)
+        elif len(result) == 2:
+            print("B")
+            reception_number = "M-" + str(int(result) + 1)
+        elif len(result) == 3:
+            print("C")
+            reception_number = "M-" + str(int(result) + 1)
+        elif len(result) == 4:
+            print("D")
+            reception_number = "M-" + str(int(result) + 1)
+    print("--------{}-------".format(reception_number))
+    return reception_number
+
+# 人数選択画面
+def select_num_people(request):
+    if request.method == 'POST':
+        form = NumPeopleForm(request.POST)
+        reception_number = select_reception_number()
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.reception_number = reception_number
+            instance.save()
+            return redirect('reception_system/select_seating_type')  # 座席型指定画面へリダイレクト
+    else:
+        form = NumPeopleForm()
+    return render(request, 'reception_system/select_num_people.html', {'form': form})
+
+
+# 座席型指定画面
+def select_seating_type(request):
+    reservation = get_object_or_404(Reception)
+    if request.method == 'POST':
+        form = SeatingTypeForm(request.POST)
+        if form.is_valid():
+            reservation.seating_type = form.cleaned_data['seating_type']
+            reservation.save()
+            return redirect('reception_system/select_seat_specification')  # 座席指定画面へリダイレクト
+    else:
+        form = SeatingTypeForm()
+    return render(request, 'reception_system/select_seating_type.html', {'form': form})
+
+# 座席指定画面
+def select_seat_specification(request):
+    reservation = get_object_or_404(Reception, id=num)
+    if request.method == 'POST':
+        form = SeatSpecificationForm(request.POST)
+        if form.is_valid():
+            reservation.seat_specification = form.cleaned_data['seat_specification']
+            reservation.save()
+            return redirect('reception_system/complete_reservation')  # 受付完了画面へリダイレクト
+    else:
+        form = SeatSpecificationForm()
+    return render(request, 'reception_system/select_seat_specification.html', {'form': form})
+
+# 受付完了画面
+def complete_reservation(request):
+    reservation = get_object_or_404(Reception, id=num)
+    reservation.is_completed = True
+    reservation.save()
+    # セッションの予約IDを削除して状態をクリア
+    del request.session['reservation_id']
+    return render(request, 'reception_system/complete_reservation.html', {'reservation': reservation})
 
 
 def language(request):
-    return render(request, 'reception_system/language.html')
+    return render(request, 'reception_system/reception_system/language.html')
 
 def complate(request):
-    return render(request, 'reception_system/complate.html')
+    return render(request, 'reception_system/reception_system/complate.html')
